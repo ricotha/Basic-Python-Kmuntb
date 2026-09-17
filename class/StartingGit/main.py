6d4888cb6eb9f912bd0fb834c4ff50741e6e868d
Rico <theeradech.tha@gmail.com>
2026-09-17 10:20:06 +0700

    StartingGit

---
+++ class/StartingGit/main.py
@@ -0,0 +1,9 @@
+print("Hello World, I'm learning Git!")
+
+while True:
+    key = input("Enter a number (or 'q' to quit): ")
+    if key == 'q':
+        print("Exiting the program. Goodbye!")
+        break
+    else:
+        print("You pressed " + key)
